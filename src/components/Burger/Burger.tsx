9721cfca9190c0946f8c52d8295c297cb895d8b5
Deividas <dpakamore@gmail.com>
2021-09-21 11:18:15 +0300

    latest commit lul

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -7,19 +7,48 @@ interface TheseProps {
 }
 
 const burger = (props: TheseProps) => {
-  const transformedIngredients = Object.keys(props.ingredients)
-  .map(igKey => {
-    return [...Array(Object.keys(props.ingredients).length)].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey}/>;
+  const transformedIngredientsNames = Object.keys(props.ingredients);
+  const transformedIngredientsValues: number[] = Object.values(props.ingredients);
+  const ingredientsJsxElements: JSX.Element[] = [];
+
+
+  if (transformedIngredientsValues.reduce((prev, current) => prev + current) === 0) {
+    ingredientsJsxElements.push(<p>Please start adding ingredients!</p>);
+  }
+
+  // creating emty arrays with length of given elements
+  const ingredientsEmtyArray = transformedIngredientsValues.map(igValue => {
+    return [...Array(igValue)]
+  })
+
+
+
+  //filling my emty array, with ingredient names
+  ingredientsEmtyArray.forEach(arr => {
+    arr.fill(transformedIngredientsNames[ingredientsEmtyArray.indexOf(arr)]);
+  });
+
+
+  //Initializing Array with JSX elements
+  ingredientsEmtyArray.forEach(arr => {
+    arr.map((ing, i) => {
+      return ingredientsJsxElements.push(<BurgerIngredient key={ing + i} type={ing} />);
     })
   });
+
+
+  
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type={"bread-top"}/>
-        {transformedIngredients}
+        {ingredientsJsxElements}
       <BurgerIngredient type={"bread-bottom"}/>
     </div>
   );
 };
 
 export default burger;
+
+function sum(arg0: any): any {
+  throw new Error("Function not implemented.");
+}
--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -2,22 +2,34 @@ import React from "react";
 import { Component } from "react";
 import MyAux from "../../hoc/MyAux";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   }
+
+  // addIngredientHandler = (type: string) => {
+  //   const oldCount = this.state.ingredients.type;
+  //   const updatedCount = oldCount + 1;
+  //   const updatedIngredients = {
+  //     ...this.state.ingredients 
+  //   };
+  //   updatedIngredients[type] = updatedCount;
+  // }
+  // https://stackoverflow.com/questions/46987816/using-state-in-react-with-typescript/46987987
+
   render () {
     return (
       <MyAux>
         <Burger ingredients={this.state.ingredients}/>
-        <div>Build Controls</div>
+        <BuildControls/>
       </MyAux>
     );
   }
